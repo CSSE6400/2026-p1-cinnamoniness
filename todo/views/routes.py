2efde273b9cb6706951494ec1df995988e162c56
cinnamoniness <cinnamoniness@gmail.com>
2026-03-01 09:34:58 +0000

    Healthy commit message:

--- todo/views/routes.py
+++ todo/views/routes.py
@@ -1,9 +1,9 @@
 from flask import Blueprint, jsonify
 api = Blueprint('api', __name__,url_prefix='/api/v1')
 
-@api.route('health')
+@api.route('/health')
 def health():
-    return jsonify({'status': 'ok'})
+    return jsonify({'status': 'ok'}),200
 
 @api.route('todos', methods=['GET'])
 def get_todos():
